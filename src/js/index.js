43174d09efe73447be7ba56fa3f4d89b18dd0715
Anjacodes <anja_schmidt7@posteo.de>
2021-11-08 16:45:37 -0600

    Add hrefs to Anchor Tags in Menu Elements, Add Event Listener to Menu Elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,22 @@
-let btnBars = document.getElementById('barsBtn');
-let mobMenu = document.getElementsByClassName('mobMenu');
-let closeIcon = document.getElementsByClassName('closeIcon');
-let listEl = document.querySelectorAll('.mobMenu > ul > li');
+let btnBars = document.getElementById("barsBtn");
+let mobMenu = document.getElementsByClassName("mobMenu");
+let closeIcon = document.getElementsByClassName("closeIcon");
+let listEl = document.querySelectorAll(".mobMenu > ul > li");
 console.log(btnBars);
 console.log(mobMenu);
 console.log(closeIcon);
 console.log(listEl);
 
-btnBars.addEventListener('click', function (){
-    mobMenu[0].style.display = "flex";
+btnBars.addEventListener("click", function () {
+  mobMenu[0].style.display = "flex";
 });
 
-closeIcon[0].addEventListener('click', function (){
+closeIcon[0].addEventListener("click", function () {
+  mobMenu[0].style.display = "none";
+});
+
+for (i of listEl) {
+  i.addEventListener("click", function () {
     mobMenu[0].style.display = "none";
-});
-
-
+  });
+}

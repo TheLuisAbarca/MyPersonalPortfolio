3522dbe2bf32f07be470b18b4418d20d8e3747f6
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-08 17:30:15 -0500

    Implement hamburguer javascript feature and close icon action.

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+let btnBars = document.getElementById('barsBtn');
+let mobMenu = document.getElementsByClassName('mobMenu');
+let closeIcon = document.getElementsByClassName('closeIcon');
+let listEl = document.querySelectorAll('.mobMenu > ul > li');
+console.log(btnBars);
+console.log(mobMenu);
+console.log(closeIcon);
+console.log(listEl);
+
+btnBars.addEventListener('click', function (){
+    mobMenu[0].style.display = "flex";
+});
+
+closeIcon[0].addEventListener('click', function (){
+    mobMenu[0].style.display = "none";
+});
+
+

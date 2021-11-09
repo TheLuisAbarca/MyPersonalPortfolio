59de9f2687e7421d7cb91f9f79c866bf089c7f37
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-09 11:43:21 -0500

    Fix linter errors on html, css and js files.

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,18 @@
-let btnBars = document.getElementById("barsBtn");
-let mobMenu = document.getElementsByClassName("mobMenu");
-let closeIcon = document.getElementsByClassName("closeIcon");
-let listEl = document.querySelectorAll(".mobMenu > ul > li");
-console.log(btnBars);
-console.log(mobMenu);
-console.log(closeIcon);
-console.log(listEl);
+const btnBars = document.getElementById('barsBtn');
+const mobMenu = document.getElementsByClassName('mobMenu');
+const closeIcon = document.getElementsByClassName('closeIcon');
+const listEl = document.querySelectorAll('.mobMenu > ul > li');
 
-btnBars.addEventListener("click", function () {
-  mobMenu[0].style.display = "flex";
+btnBars.addEventListener('click', () => {
+  mobMenu[0].style.display = 'flex';
 });
 
-closeIcon[0].addEventListener("click", function () {
-  mobMenu[0].style.display = "none";
+closeIcon[0].addEventListener('click', () => {
+  mobMenu[0].style.display = 'none';
 });
 
-for (i of listEl) {
-  i.addEventListener("click", function () {
-    mobMenu[0].style.display = "none";
+listEl.forEach((el) => {
+  el.addEventListener('click', () => {
+    mobMenu[0].style.display = 'none';
   });
-}
+});

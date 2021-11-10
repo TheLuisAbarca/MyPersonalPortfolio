763c344bb3872db20464154696fdd2317db50785
Anjacodes <anja_schmidt7@posteo.de>
2021-11-10 15:22:33 -0600

    Implement Form Validation for Email

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,27 @@ const mobMenu = document.getElementsByClassName('mobMenu');
 const closeIcon = document.getElementsByClassName('closeIcon');
 const listEl = document.querySelectorAll('.mobMenu > ul > li');
 const bodyel = document.querySelector('body');
+const formEl = document.getElementById('contactForm');
+const emailMsg = formEl.querySelector('small');
+const emailForm =  formEl.querySelector('input[type="email"]');
+
+formEl.addEventListener('submit', (e) => {
+  console.log(hasUpperCase(emailForm.value));
+  if (hasUpperCase(emailForm.value)) {
+    e.preventDefault();
+    emailMsg.style.display = 'block';
+    emailMsg.textContent = 'Email needs to be lowercase';
+    console.log(emailMsg);
+  } else {
+    emailMsg.style.display = 'none';
+  }
+});
+
+function hasUpperCase(str) {
+  const regExp = /[A-Z]/;
+  return regExp.test(str);
+}
+
 
 btnBars.addEventListener('click', () => {
   mobMenu[0].style.display = 'flex';
